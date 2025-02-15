47a0eb1be20b4c826e2c32289571cea1a1700549
Bulba39dfgcesdfcgre <piska4540@gmail.com>
2025-02-15 14:09:44 +0400

    Delete valentines_app/views.py

--- valentines_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'valentines_app/home.html')
-
-def success(request):
-    return render(request, 'valentines_app/success.html')
